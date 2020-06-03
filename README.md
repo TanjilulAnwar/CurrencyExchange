aef867a0099ffdf7876968a4c58fca3249d9c0de
tanjilul <tanjilulanwar@gmail.com>
2020-06-03 10:30:03 +0600

    Negative Input Unvalidated

---
+++ README.md
@@ -0,0 +1,18 @@
+# CurrencyExchange
+Currency converter with Fixer.io API (Free version) supporting 170 foreign exchange
+
+## Installation
+
+Use the API key for free (1000 hit/mo) from [Fixer.io](https://fixer.io/documentation) 
+
+```bash
+http://data.fixer.io/api/latest?access_key = API_KEY
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ js/main.js
@@ -0,0 +1,36 @@
+const endpoint = 'http://data.fixer.io/api/latest?access_key=8d34092eb82e5293bec6c3f0cdd15c7c';
+
+const fromCurrency = document.querySelector(".fromInput");
+const toCurrency = document.querySelector(".toInput");
+var from ;
+var to;
+
+document.getElementById('dropDownFrom').onchange = function() {
+   from =this.value;
+   
+};
+
+document.getElementById('dropDownTo').onchange = function() {
+   to =this.value;
+
+};
+
+
+function exchange(){
+fetch(endpoint)
+.then(response => response.json())
+.then(date=>{
+	eval("firstAmount= date.rates."+from);
+	eval("secondAmount= date.rates."+to);
+	toCurrency.value= ((secondAmount/firstAmount)*fromCurrency.value).toFixed(2);
+});
+
+
+}
+
+
+
+document.querySelector(".convertButton").addEventListener("click",exchange);
+//fromCurrency.addEventListener("input",conversor);
+
+
